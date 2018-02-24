3431254d23c5acad3d22e893ddfba1d7fcf39e5e
Ana Ortega <anaa0hh@icloud.com>
2018-02-24 13:17:59 -0800

    instances fixed, added correct properties

--- JS/results.js
+++ JS/results.js
@@ -11,46 +11,48 @@ var allPups =[];
 var currProducts = [0,0,0];
 
 //constructor for dogs
-function Dog(breed, sizeOfPup, pupLifestyle, allergies, pupPurpose, noiseTolerance, furLength) {
-    this.breed = breed;
-    this.sizeOfPup = sizeOfPup;
-    this.pupLifestyle = pupLifestyle;
-    this.allergies = allergies;
-    this.pupPurpose = pupPurpose;
-    this.noiseTolerance = noiseTolerance;
-    this.furLength = furLength;
-    this.pupScore = 0;
-    allPups.push(this);
+function Dog(breed, sizeOfPup, pupLifestyle, allergies, pupPurpose, noiseTolerance, furLength, filePath) {
+  this.breed = breed;
+  this.sizeOfPup = sizeOfPup;
+  this.pupLifestyle = pupLifestyle;
+  this.allergies = allergies;
+  this.pupPurpose = pupPurpose;
+  this.noiseTolerance = noiseTolerance;
+  this.furLength = furLength;
+  this.pupScore = 0;
+  this.filePath=filePath;
+
+  allPups.push(this);
 }
 
 //*********MAINLINE*********
 
 //Retrieve quiz results
 (function getLocalStorage() {
-    if (localStorage.quizResults) {
-        console.log('we want quiz results');
-        var strQuizResults = localStorage.getItem('quizResults');
-        var quizResults = JSON.parse(strQuizResults);
-        console.log(quizResults);
-    }
+  if (localStorage.quizResults) {
+    console.log('we want quiz results');
+    var strQuizResults = localStorage.getItem('quizResults');
+    var quizResults = JSON.parse(strQuizResults);
+    console.log(quizResults);
+  }
 })();
 
 //**********FUNCTIONS START HERE *****************/
 //Build Objects
 function instantiateDogs() {
-    new Dog('newfy', newfy.jpg, 0, 0);
-    new Dog('doxie', doxie.jpg, 0, 0);
-    new Dog('golden', golden.jpg, 0, 0);
+  new Dog('Newfoundland', 'large' , 'sedentary', 'no', 'companionship', 'no-barking', 'some-fur', 'newfy.jpg');
+  new Dog('Smooth Coat Dachsund', 'small' , 'somewhat-active', 'yes', 'companionship', 'some-barking', 'min-fur', 'doxie.jpg');
+  new Dog('Golden Retriever', 'large' , 'active', 'no', 'service', 'some-barking', 'some-fur', 'golden.jpg');
 
 }
 
-function scorePups() {
-    for (var i=0; i < allPups.length; i++) {
-        //we are starting at one because we don't want to incluse the first question in score
-        for (var j=1; j < quizResults.length; j++) {
-            
 
-        }
+function scorePups() {
+  for (var i=0; i < allPups.length; i++) {
+    //we are starting at one because we don't want to incluse the first question in score
+    for (var j=1; j < quizResults.length; j++) {
+        
     }
+  }
 }
 
